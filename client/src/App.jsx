bdd2de8c09947da2501f9c08b5994d0dd2722e02
parth-negi00 <app9.nessco@gmail.com>
2026-01-21 15:36:56 +0530

    changed app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css';
 import Signup from './components/Signup';
-import Login from "./components/Login";
+// import Login from "./components/Login";  <-- remove this import
 
 function App() {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>MERN Auth Example</h1>
-      <Signup/>
-      <Login />
+      <Signup />
+      {/* <Login /> <-- remove this */}
     </div>
   );
 }
-
 
 export default App;
