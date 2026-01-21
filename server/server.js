6f214e4efcd1f76a63e6aa92917432dd98117aac
parth-negi00 <app9.nessco@gmail.com>
2026-01-21 16:54:58 +0530

    set port after jwt_secret setup

--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
