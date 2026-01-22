56cf3e2c2b68c831041acc6eab7efba77bcea6f2
parth-negi00 <app9.nessco@gmail.com>
2026-01-22 17:37:06 +0530

    -A

---
+++ client/src/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", color: "#fff" }}>
+      <h1>Welcome to Dashboard 🚀</h1>
+      <p>This is where your drag & drop form builder will live.</p>
+    </div>
+  );
+}
